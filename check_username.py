4633190d33d927956f4c095d167be3223389a88c
Pikai Mandal <pikai.mandal2001@outlook.com>
2022-12-19 13:00:28 +0530

    Update check_username.py

--- check_username.py
+++ check_username.py
@@ -9,7 +9,7 @@ consumer_secret = "F5MDKIWgjgBhN4UEqnMEG01mfo95J8sNKyTUaDtVy5NlfbkUHo"
 # Use the requests library to send a request to the Twitter API to retrieve an
 # access token
 response = requests.post(
-    "https://api.twitter.com/oauth2/token",
+    "https://api.twitter.com/oauth2/AAAAAAAAAAAAAAAAAAAAACxMkgEAAAAAovUYIgSrZ%2BwuEdF%2Fe%2BJnXywYnuM%3DitPR7vr5DvnjPlEKToqyBrQ2V5tAyeiHElxqrQzGaJQX5vFpp3",
     auth=(consumer_key, consumer_secret),
     data={"grant_type": "client_credentials"}
 )
