98dbbf7d61f267d05653e8774a47d5f5f168555e
Pikai Mandal <pikai.mandal2001@outlook.com>
2022-12-19 13:34:18 +0530

    Update check_username.py

--- check_username.py
+++ check_username.py
@@ -9,16 +9,16 @@ consumer_secret = "F5MDKIWgjgBhN4UEqnMEG01mfo95J8sNKyTUaDtVy5NlfbkUHo"
 # Use the requests library to send a request to the Twitter API to retrieve an
 # access token
 response = requests.post(
-    "https://api.twitter.com/oauth2/AAAAAAAAAAAAAAAAAAAAACxMkgEAAAAAovUYIgSrZ%2BwuEdF%2Fe%2BJnXywYnuM%3DitPR7vr5DvnjPlEKToqyBrQ2V5tAyeiHElxqrQzGaJQX5vFpp3",
+    "https://api.twitter.com/oauth2/token",
     auth=(consumer_key, consumer_secret),
     data={"grant_type": "client_credentials"}
 )
 
 # Extract the access token from the response
-access_token = response.json()["access_token"]
+access_token = response.json()["AAAAAAAAAAAAAAAAAAAAACxMkgEAAAAAovUYIgSrZ%2BwuEdF%2Fe%2BJnXywYnuM%3DitPR7vr5DvnjPlEKToqyBrQ2V5tAyeiHElxqrQzGaJQX5vFpp3"]
 
 # Use the access token to authenticate requests to the Twitter API
-auth_header = f"Bearer {access_token}"
+auth_header = f"Bearer {AAAAAAAAAAAAAAAAAAAAACxMkgEAAAAAovUYIgSrZ%2BwuEdF%2Fe%2BJnXywYnuM%3DitPR7vr5DvnjPlEKToqyBrQ2V5tAyeiHElxqrQzGaJQX5vFpp3}"
 
 # Use the requests library to send a request to the Twitter API to check if a
 # username is available
